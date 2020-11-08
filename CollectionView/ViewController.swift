9f731735eb6b900d684378747a40f32e2d248c86
TheinDev <theinnaingdev@gmail.com>
2020-11-08 21:18:09 +0630

    ui collection view is  added

--- CollectionView/ViewController.swift
+++ CollectionView/ViewController.swift
@@ -8,8 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UICollectionViewDelegate,
+UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        
+        return 100;
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        var uiCel = UICollectionViewCell();
+        uiCel.backgroundColor = UIColor.green
+        return uiCel;
+    }
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1;
+    }
 
+    @IBOutlet weak var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
